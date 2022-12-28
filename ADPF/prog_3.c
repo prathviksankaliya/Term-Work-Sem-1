4b2d4da1c4b956839df71439a4ecd2382c2dfe37
Shadow <prathviksankaliya@gmail.com>
2022-12-28 10:04:57 +0530

    DBMS DONE

--- ADPF/prog_3.c
+++ ADPF/prog_3.c
@@ -1,112 +1,112 @@
-#include <stdio.h>
-#include <string.h>
-
-void sort(char str[], int n)
-{
-    char temp;
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n - 1; j++)
-        {
-            if (str[j] > str[j + 1])
-            {
-                temp = str[j];
-                str[j] = str[j + 1];
-                str[j + 1] = temp;
-            }
-        }
-    }
-}
-int fun(char str1[], char str2[], int str1len, int str2len)
-{
-    int count = 0;
-    int flag = 0;
-    for (int i = 0; i < str1len; i++)
-    {
-        for (int j = 0; j < str2len; j++)
-        {
-            if (str1[i] == str2[j])
-            {
-                flag = 1;
-                if (i == j)
-                {
-                    count++;
-                }
-            }
-            else if (j == str2len - 1 && flag == 0)
-            {
-                return -1;
-            }
-        }
-    }
-    return count;
-}
-int main()
-{
-    char str1[100];
-    char str2[100];
-    printf("Enter the First String : ");
-    scanf("%s",str1);
-
-    printf("Enter the Second String : ");
-    scanf(" %s",str2);
-    if (strlen(str1) != strlen(str2))
-    {
-        printf("%d", -1);
-    }
-    else
-    {
-        char str1Temp[strlen(str1)];
-        char str2Temp[strlen(str2)];
-        strcpy(str1Temp, str1);
-        strcpy(str2Temp, str2);
-        sort(str1Temp, strlen(str1));
-        sort(str2Temp, strlen(str2));
-        if (strcmp(str1Temp, str2Temp) == 0)
-        {
-            printf("%d", fun(str1, str2, strlen(str1), strlen(str2)));
-        }else{
-            printf("%d", -1);
-        }
-    }
-}
-
 // #include <stdio.h>
 // #include <string.h>
 
-// int Check_anagram(char str1[], char str2[]) {
-//     int s_freq[26] = {0};
-//     int t_freq[26] = {0};
-//     for (int i = 0; i < strlen(str1); i++) {
-//         s_freq[str1[i] - 'a']++;
-//     }
-//     for (int i = 0; i < strlen(str2); i++) {
-//         t_freq[str2[i] - 'a']++;
-//     }
-//     for (int i = 0; i < 26; i++) {
-//         if (s_freq[i] != t_freq[i]) {
-//             return -1;
+// void sort(char str[], int n)
+// {
+//     char temp;
+//     for (int i = 0; i < n; i++)
+//     {
+//         for (int j = 0; j < n - 1; j++)
+//         {
+//             if (str[j] > str[j + 1])
+//             {
+//                 temp = str[j];
+//                 str[j] = str[j + 1];
+//                 str[j + 1] = temp;
+//             }
 //         }
-    
+//     }
+// }
+// int fun(char str1[], char str2[], int str1len, int str2len)
+// {
 //     int count = 0;
-//     for (int i = 0; i < strlen(str1); i++) {
-//         if (str1[i] == str2[i]) {
-//             count++;
+//     int flag = 0;
+//     for (int i = 0; i < str1len; i++)
+//     {
+//         for (int j = 0; j < str2len; j++)
+//         {
+//             if (str1[i] == str2[j])
+//             {
+//                 flag = 1;
+//                 if (i == j)
+//                 {
+//                     count++;
+//                 }
+//             }
+//             else if (j == str2len - 1 && flag == 0)
+//             {
+//                 return -1;
+//             }
 //         }
 //     }
 //     return count;
 // }
-// }
-// int main(){
-//     char str1[50];
-//     char str2[50];
-
+// int main()
+// {
+//     char str1[100];
+//     char str2[100];
 //     printf("Enter the First String : ");
 //     scanf("%s",str1);
 
 //     printf("Enter the Second String : ");
 //     scanf(" %s",str2);
-
-//     printf("%d ",Check_anagram(str1,str2));
-//     return 0;
+//     if (strlen(str1) != strlen(str2))
+//     {
+//         printf("%d", -1);
+//     }
+//     else
+//     {
+//         char str1Temp[strlen(str1)];
+//         char str2Temp[strlen(str2)];
+//         strcpy(str1Temp, str1);
+//         strcpy(str2Temp, str2);
+//         sort(str1Temp, strlen(str1));
+//         sort(str2Temp, strlen(str2));
+//         if (strcmp(str1Temp, str2Temp) == 0)
+//         {
+//             printf("%d", fun(str1, str2, strlen(str1), strlen(str2)));
+//         }else{
+//             printf("%d", -1);
+//         }
+//     }
 // }
+
+#include <stdio.h>
+#include <string.h>
+
+int Check_anagram(char str1[], char str2[]) {
+    int s_freq[26] = {0};
+    int t_freq[26] = {0};
+    for (int i = 0; i < strlen(str1); i++) {
+        s_freq[str1[i] - 'a']++;
+    }
+    for (int i = 0; i < strlen(str2); i++) {
+        t_freq[str2[i] - 'a']++;
+    }
+    for (int i = 0; i < 26; i++) {
+        if (s_freq[i] != t_freq[i]) {
+            return -1;
+        }
+    
+    int count = 0;
+    for (int i = 0; i < strlen(str1); i++) {
+        if (str1[i] == str2[i]) {
+            count++;
+        }
+    }
+    return count;
+}
+}
+int main(){
+    char str1[50];
+    char str2[50];
+
+    printf("Enter the First String : ");
+    scanf("%s",str1);
+
+    printf("Enter the Second String : ");
+    scanf(" %s",str2);
+
+    printf("%d ",Check_anagram(str1,str2));
+    return 0;
+}
--- DBMS/DBMS Termwork.sql
+++ DBMS/DBMS Termwork.sql
@@ -2,20 +2,20 @@ create table item (itemid int primary key, iname varchar(20), price int);
 
 create table customer (custid int primary key, customername varchar(20), city varchar(20));
 
-create table order_master (orderid int primary key, orderdate date, custid int,foreign key(custid) references customer(custid));
+create table order_master (orderid int primary key, orderdate date, custid int, foreign key(custid) references customer(custid));
 
-create table order_detail (orderid int , itemid int, foreign key (orderid) references order_master(orderid), foreign key (itemid) references item(itemid));
+create table order_detail (orderid int, itemid int,foreign key (orderid) references order_master(orderid), foreign key (itemid) references item(itemid));
 
-insert into item values(1234, 'shirt', 500), (1235, 't-shirt', 1050), (1236, 'jeans', 750), (1237, 'shorts', 950), (1238, 'lycra', 1100), (1239, 'mobile', 12000);
+insert into item values(2035, 'jacket', 450), (2036, 't-shirt', 950), (2037, 'jeans', 750), (2038, 'blanket', 1950), (2039, 'lycra', 1000), (2040, 'mobile', 12000);
 
-insert into customer values (01, 'savan', 'Rajkot'), (02,'tapu', 'Rajula'), (03,'Akshay', 'Amreli'), (04, 'Parth', 'Limbdi'), (05, 'Rushit', 'Bhavnagar');
+insert into customer values (1, 'savan', 'Rajkot'), (2,'tapu', 'Rajula'), (3,'Akshay', 'Rajkot'), (4, 'Parth', 'Limbdi'), (5, 'Rushit', 'Bhavnagar');
 
-insert into order_master (orderid, custid, orderdate) values (121, 03, '2022-11-11'), (122, 02, '2022-04-03'), (123, 01, '2022-02-06'), (124, 01, '2022-03-11'), (125, 04, '2022-11-06');
+insert into order_master (orderid, custid, orderdate) values (121, 3, '2022-11-11'), (122, 2, '2022-04-03'), (123, 1, '2022-02-06'), (124, 1, '2022-03-11'), (125, 4, '2022-11-06');
 
-insert into order_detail values(121, 1238), (122, 1238), (123, 1236), (124, 1237), (125, 1235), (123, 1236), (121, 1234);
+insert into order_detail values(121, 2038), (122, 2038), (123, 2036), (124, 2037), (125, 2035);
 
 -- 1
-select * from item where price > 10000;
+select * from item where price > 1000;
 
 -- 2 
 select orderid from order_master where custid in (select custid from customer where city = 'baroda');
@@ -27,7 +27,7 @@ select orderid from order_detail where itemid in (select iname from item where i
 select customername from customer where custid not in (select custid from order_master);
 
 -- 5
-select iname from item where itemid in (select itemid from order_detail where orderid in (select orderid from order_master where orderdate < '2020-10-23'));
+select iname from item where itemid in (select itemid from order_detail where orderid in (select orderid from order_master where orderdate < '2022-10-23'));
 
 -- 6
 select iname from item where itemid in (select itemid from order_detail where orderid in (select orderid from order_master where custid = 01));
@@ -41,10 +41,12 @@ update item set price = price + 200 where iname = 'mobile';
 select * from item where iname = 'mobile';
 
 -- 9
--- create procedure pro1 (in iname varchar(20), out  orderid int)
--- begin
--- 	select orderid from order_master where itemid in ()
+create procedure proa(in name varchar(20))
+begin
+ select orderid from order_detail where itemid in(select itemid from item where iname = name);
+end $$
  
+ call proa('jacket');
 -- 10
 create function funct(order_id int)
 returns int
